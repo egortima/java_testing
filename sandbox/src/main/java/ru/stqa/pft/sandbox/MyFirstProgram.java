5143822a29a0e03781005aae8b35254df9ad2345
egortima <38738280+egortima@users.noreply.github.com>
2019-05-10 23:51:19 +0300

    Add some methods

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,51 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram{
+public class MyFirstProgram {
 
-  public static void main(String[] args) {
-    System.out.println("Hello, world!");
+  public static void main(String[] args)
+  {
+    hello("world");
+    hello("user");
+    hello("Egor");
+
+    double l = 5;
+    System.out.println("square area with sides " + l + " = " + area(l));
+
+
+    double a = 4;
+    double b = 10;
+
+    System.out.println("rectangle area with sides " + a + " and " + b + " = " + area(a,b));
+
+//    int l =8;
+//    int s = l * l;
+//    System.out.println("square area with sides " + l + " = " + s);
+//
+//    System.out.println(2+2);
+//    System.out.println(2*2);
+//    System.out.println(2/2);
+//    System.out.println(2-2);
+//    System.out.println(1/2);
+//    System.out.println(1.0/2);
+//    System.out.println("2"+"2");
+//    System.out.println("2"+2);
+//    System.out.println((2+2)*2);
+//    System.out.println("2+2 = " + (2+2));
+//    System.out.println("2+2 = " + 2+2);
+
+  }
+
+  public static void hello( String somebody)
+  {
+    System.out.println("Hello" + somebody + "!");
+  }
+
+  public static double area(double len)
+  {
+    return len*len;
+  }
+  public static double area(double a, double b)
+  {
+    return a*b;
   }
 }
