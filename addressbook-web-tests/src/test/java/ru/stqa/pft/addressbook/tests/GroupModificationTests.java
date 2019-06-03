e9b1ecc08e572a479c175577a09098e78021b4f6
egortima <38738280+egortima@users.noreply.github.com>
2019-06-03 12:11:41 +0300

    Add fluent interface

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase {
   public void ensurePreconditions() {
     app.goTo().groupPage("groups");
     if (app.group().list().size() == 0) {
-      app.group().create(new GroupData( "test1", null, null));
+      app.group().create(new GroupData().withName("test1"));
     }
   }
 
@@ -22,7 +22,9 @@ public class GroupModificationTests extends TestBase {
 
     List<GroupData> before = app.group().list();
     int index = before.size() - 1;
-    GroupData group = new GroupData(before.get(index).getId(),"test1", "Hello!", "Wow");
+    GroupData group = new GroupData()
+            .withId(before.get(index).getId()).withName("test1").withHeader("Hello!").withHeader("Wow");
+
     app.group().modify(index, group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size());
